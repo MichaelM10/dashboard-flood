1851acecfd7ad635f8a11fd10790391234b8bf9e
Michael Marcelino <55041641+MichaelM10@users.noreply.github.com>
2023-04-11 02:03:20 +0700

    Modify View & SensorController Update

--- app/Http/Controllers/SensorController.php
+++ app/Http/Controllers/SensorController.php
@@ -6,6 +6,7 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
 use App\Models\Bookmark;
 use App\Models\Sensor;
+use Input;
 
 class SensorController extends Controller
 {
@@ -40,7 +41,7 @@ class SensorController extends Controller
             return redirect()->back()->with('error', "SensorKu with that Id is not found, please check the packaging/casing for the correct Id")->withInput();
         }
     }
-    
+
     public function indexModify(Request $request){
 
         $sensor = json_decode($request->sensor);
@@ -57,7 +58,9 @@ class SensorController extends Controller
         $sensor->visibility = $request->visibility;
         $sensor->access_password = $request->access_password;
         $sensor->update();
-        return view("modify.modify",['sensor' => $sensor])->with('success', "Data saved successfully");
-        // return redirect('sensor/modify-back')->with('success', "Sensor successfully modified")->with('sensor', $sensor);
+        $success="Data saved successfully!";
+        
+        $request->flash();
+        return view("modify.modify-back",['success' => $success]);
     }
 }
