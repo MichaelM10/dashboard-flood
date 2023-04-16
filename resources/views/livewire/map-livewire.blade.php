6cf779416b759e60f0517b036862426253b8846e
Michael Marcelino <55041641+MichaelM10@users.noreply.github.com>
2023-04-16 13:45:59 +0700

    Mapbox Key moved to .env

--- resources/views/livewire/map-livewire.blade.php
+++ resources/views/livewire/map-livewire.blade.php
@@ -1,13 +1,12 @@
 @extends('layouts.app')
 @section('content')
 <div>
-<div id='map' style='width: 400px; height: 300px;'></div>
+<div wire-ignore id='map' style='width: 400px; height: 300px;'></div>
 </div>
 @endsection
 @push('scripts')
-<div id='map' style='width: 400px; height: 300px;'></div>
 <script>
-  mapboxgl.accessToken = 'pk.eyJ1Ijoic2hhcmRzaXgiLCJhIjoiY2xnZzdtMHNtMDk2NDNrcnZwbGFqMGU3NSJ9.t4ixFUVLrcygdu0ZvC3a4Q';
+  mapboxgl.accessToken = '{{env("MAPBOX_KEY")}}';
   var map = new mapboxgl.Map({
     container: 'map',
     style: 'mapbox://styles/mapbox/streets-v11'
