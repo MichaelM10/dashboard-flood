12cc6dcbc225987a7528a2b747d0a248cdf58014
Michael Marcelino <55041641+MichaelM10@users.noreply.github.com>
2023-06-02 00:54:27 +0700

    Revert "Allow http 2"
    This reverts commit 879853cb95780ab995686d1ff380939c6ffd4b5c.

--- app/Http/Middleware/AllowHTTP.php
+++ app/Http/Middleware/AllowHTTP.php
@@ -6,7 +6,7 @@ use Closure;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
 
-class AllowHTTP extends Middleware
+class AllowHTTP
 {
     /**
      * Handle an incoming request.
